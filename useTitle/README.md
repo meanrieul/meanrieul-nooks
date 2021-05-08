7466893922f500e72b0bf6fc82e84bc93193b4cd
meanrieul <wp1270@users.noreply.github.com>
2021-05-08 22:35:49 +0900

    useTitle

---
+++ useTitle/README.md
@@ -0,0 +1,3 @@
+# @meanrieul-nooks/use-title
+
+React Hook to update your document's title
--- useTitle/index.js
+++ useTitle/index.js
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import useTitle from "./useTitle";
+import { useState, useEffect } from "react";
 
-const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
-  return (
-    <div className="App">
-      <div>Hi</div>
-    </div>
-  );
+export const UseTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
 };
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
